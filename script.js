cda5e898040e4a68cdfaec5422dbe956103513ed
Yuri <y.n.d.517@icloud.com>
2026-07-05 17:37:00 +0800

    PWA対応

---
+++ script.js
@@ -0,0 +1,377 @@
+const themes = {
+  vegetables: [
+    {
+      name: "にんじん",
+      image: "images/food/carrot.png"
+    },
+    {
+      name: "とまと",
+      image: "images/food/tomato.png"
+    },
+     {
+      name: "じゃがいも",
+      image: "images/food/poteto.png"
+    },
+    {
+      name: "かぼちゃ",
+      image: "images/food/kabocha.png"
+    },
+    {
+      name: "さつまいも",
+      image: "images/food/satsumaimo.png"
+    },
+    {
+      name: "たまねぎ",
+      image: "images/food/tamanegi.png"
+    },
+    {
+      name: "こまつな",
+      image: "images/food/komatsuna.png"
+    },
+    {
+      name: "らでぃっしゅ",
+      image: "images/food/radish.png"
+    },
+    {
+      name: "すいか",
+      image: "images/food/suika.png"
+    },
+     {
+      name: "めろん",
+      image: "images/food/melon.png"
+    },
+    {
+      name: "みかん",
+      image: "images/food/mikan.png"
+    },
+    {
+      name: "もも",
+      image: "images/food/momo.png"
+    },
+    {
+      name: "りんご",
+      image: "images/food/apple.png"
+    },
+    {
+      name: "さくらんぼ",
+      image: "images/food/cherry.png"
+    },
+    {
+      name: "いちご",
+      image: "images/food/ichigo.png"
+    },
+  ],
+
+  vehicles: [
+    {
+      name: "くるま",
+      image: "images/vehicle/car.png"
+    },
+    {
+      name: "でんしゃ",
+      image: "images/vehicle/train.png"
+    },
+    {
+      name: "ばす",
+      image: "images/vehicle/bus.png"
+    },
+    {
+      name: "ふね",
+      image: "images/vehicle/fune.png"
+    },
+    {
+      name: "きゅうきゅうししゃ",
+      image: "images/vehicle/kyukyusya.png"
+    },
+    {
+      name: "ろーどろーらー",
+      image: "images/vehicle/roadroller.png"
+    },
+    {
+      name: "しんかんせん",
+      image: "images/vehicle/shinkansen.png"
+    },
+    {
+      name: "しょうぼうしゃ",
+      image: "images/vehicle/syoubousya.png"
+    },
+    {
+      name: "たくしー",
+      image: "images/vehicle/taxi.png"
+    },
+    {
+      name: "ごみしゅうしゅうしゃ",
+      image: "images/vehicle/gomisyusyusya.png"
+    },
+    {
+      name: "くれーんしゃ",
+      image: "images/vehicle/crane.png"
+    },
+    {
+      name: "ぱとかー",
+      image: "images/vehicle/minipato.png"
+    },
+    {
+      name: "ひこうき",
+      image: "images/vehicle/airplane.png"
+    }
+  ],
+  iro: [
+    {
+      name: "あか",
+      image: "images/iro/red.png"
+    },
+    {
+      name: "くろ",
+      image: "images/iro/black.png"
+    },
+    {
+      name: "あお",
+      image: "images/iro/blue.png"
+    },
+    {
+      name: "ちゃいろ",
+      image: "images/iro/brown.png"
+    },
+    {
+      name: "みどり",
+      image: "images/iro/green.png"
+    },{
+      name: "きみどり",
+      image: "images/iro/lime.png"
+    },
+    {
+      name: "おれんじ",
+      image: "images/iro/orange.png"
+    },
+    {
+      name: "むらさき",
+      image: "images/iro/purple.png"
+    },
+    {
+      name: "みずいろ",
+      image: "images/iro/skyblue.png"
+    },{
+      name: "しろ",
+      image: "images/iro/white.png"
+    },
+    {
+      name: "きいろ",
+      image: "images/iro/yellow.png"
+    }
+  ],
+
+form: [
+    {
+      name: "まる",
+      image: "images/form/maru.png"
+    },
+    {
+      name: "さんかく",
+      image: "images/form/sankakukei.png.png"
+    },
+    {
+      name: "しかく",
+      image: "images/form/shikakukei.png"
+    },
+    {
+      name: "ひしがた",
+      image: "images/form/hishigata.png"
+    },
+    {
+      name: "はーと",
+      image: "images/form/heart.png"
+    },
+    {
+      name: "ほし",
+      image: "images/form/star.png"
+    },
+    {
+      name: "くろーばー",
+      image: "images/form/clover.png"
+    },
+  ],
+  animal: [
+    {
+      name: "ぶた",
+      image: "images/animal/buta.png"
+    },
+     {
+      name: "ひつじ",
+      image: "images/animal/hitsuji.png"
+    },
+     {
+      name: "いぬ",
+      image: "images/animal/inu.png"
+    },
+     {
+      name: "かば",
+      image: "images/animal/kaba.png"
+    },
+     {
+      name: "こあら",
+      image: "images/animal/koala.png"
+    },
+     {
+      name: "くま",
+      image: "images/animal/kuma.png"
+    },
+     {
+      name: "らいおん",
+      image: "images/animal/lion.png"
+    },
+     {
+      name: "ねこ",
+      image: "images/animal/neko.png"
+    },
+     {
+      name: "ぱんだ",
+      image: "images/animal/panda.png"
+    },
+     {
+      name: "ぺんぎん",
+      image: "images/animal/penguin.png"
+    },
+     {
+      name: "りす",
+      image: "images/animal/.risupng"
+    },
+     {
+      name: "さる",
+      image: "images/animal/saru.png"
+    },
+     {
+      name: "とら",
+      image: "images/animal/tora.png"
+    },
+     {
+      name: "うま",
+      image: "images/animal/uma.png"
+    },
+     {
+      name: "うさぎ",
+      image: "images/animal/usagi.png"
+    },
+     {
+      name: "ぞう",
+      image: "images/animal/zou.png"
+    },
+  ],
+  character: [
+    {
+      name: "ミッキーマウス",
+      image: "images/character/mickey.png"
+    },
+    {
+      name: "ミニーマウス",
+      image: "images/character/minnie.png"
+    },
+    {
+      name: "ドナルドダック",
+      image: "images/character/donald.png"
+    },
+    {
+      name: "デイジーダック",
+      image: "images/character/daisy.png"
+    },
+    {
+      name: "グーフィー",
+      image: "images/character/goofy.png"
+    },
+    {
+      name: "プルート",
+      image: "images/character/pluto.png"
+    },
+  ]
+};
+
+let currentTheme = [];
+let currentIndex = 0;
+
+const themeScreen = document.getElementById("theme-screen");
+const cardScreen = document.getElementById("card-screen");
+
+const image = document.getElementById("card-image");
+const nameArea = document.getElementById("card-name");
+
+const themeButtons =
+  document.querySelectorAll(".theme-btn");
+
+function showCard() {
+  const card = currentTheme[currentIndex];
+
+  image.src = card.image;
+  image.alt = card.name;
+
+  nameArea.textContent = "";
+}
+
+function speak(text) {
+  const speech =
+    new SpeechSynthesisUtterance(text);
+
+  speech.lang = "ja-JP";
+
+  speechSynthesis.cancel();
+  speechSynthesis.speak(speech);
+}
+
+themeButtons.forEach(button => {
+  button.addEventListener("click", () => {
+
+    const themeName =
+      button.dataset.theme;
+
+    currentTheme = [...themes[themeName]];
+
+    // ランダムに並び替え
+    currentTheme.sort(() => Math.random() - 0.5);
+
+    currentIndex = 0;
+
+    themeScreen.classList.add("hidden");
+    cardScreen.classList.remove("hidden");
+
+    showCard();
+  });
+});
+
+image.addEventListener("click", () => {
+  const card = currentTheme[currentIndex];
+
+  nameArea.textContent = card.name;
+  speak(card.name);
+});
+
+document
+  .getElementById("next-btn")
+  .addEventListener("click", () => {
+
+    currentIndex++;
+
+    if (currentIndex >= currentTheme.length) {
+      currentIndex = 0;
+
+      currentTheme.sort(
+        () => Math.random() - 0.5
+      );
+    }
+
+    showCard();
+  });
+
+document
+  .getElementById("back-btn")
+  .addEventListener("click", () => {
+
+    cardScreen.classList.add("hidden");
+    themeScreen.classList.remove("hidden");
+  });
+
+  if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./service-worker.js")
+    .then(() => {
+      console.log("Service Worker 登録完了");
+    });
+}
