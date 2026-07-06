c20ee593c0960dc11a221a631a17ba53815a7e58
Yuri <y.n.d.517@icloud.com>
2026-07-06 12:04:41 +0800

    画像自動キャッシュ

--- service-worker.js
+++ service-worker.js
@@ -24,3 +24,54 @@ self.addEventListener("fetch", event => {
       })
   );
 });
+const CACHE_NAME = "flashcard-v3";
+
+const STATIC_FILES = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// 最初に基本ファイルを保存
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(STATIC_FILES);
+    })
+  );
+});
+
+// 古いキャッシュを削除
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+// 読み込んだ画像などを自動でキャッシュ
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(cachedResponse => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).then(networkResponse => {
+        return caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      });
+    })
+  );
+});
